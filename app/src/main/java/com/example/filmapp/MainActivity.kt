0fbebcff745f548e92bb4f85820cd9c7756ea5ac
Josema2000 <manolo.futbol11@gmail.com>
2021-11-08 19:50:31 +0100

    Primera version proyecto

--- app/src/main/java/com/example/filmapp/MainActivity.kt
+++ app/src/main/java/com/example/filmapp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.filmapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("manolo", "La aplicacion se ha creado")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d("manolo", "La aplicacion continua")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.d("manolo", "La aplicación se ha detenido")
+    }
+
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.d("manolo", "La aplicación se ha borrado")
     }
 }
