a17e808d74a1eca9eb586f01bd9401a39e57c7d2
Josema2000 <manolo.futbol11@gmail.com>
2022-01-17 20:35:31 +0100

    Ejercicio 9.4

--- data/src/main/java/com/example/data/Android/CastDto.kt
+++ data/src/main/java/com/example/data/Android/CastDto.kt
@@ -3,6 +3,6 @@ package com.example.data.Android
 import com.google.gson.annotations.SerializedName
 
 data class CastDto(
-    @SerializedName("cast") val role: String,
+    @SerializedName("known_for_department") val role: String,
     @SerializedName("name") val name: String
 )
